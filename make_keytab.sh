f48cc84665ebadc7aff704f3a8fdb9583c575077
Stéphane Lapie <darksoul@darkbsd.org>
2021-10-19 18:11:35 +0900

    Add a helper script to create keytabs

---
+++ make_keytab.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd `dirname $0`
+DIR=`pwd`
+SCRIPT=`basename $0`
+
+usage()
+{
+	echo "Usage: KRB5_CONFIG=/path/krb5.conf $0 user [<REALM1..N>] < password-from-stdin"
+	exit 1
+}
+
+KRB_USERNAME=$1
+if [ -z "$KRB_USERNAME" ] ; then usage ; fi
+shift
+KRB_REALMS=$@
+read KRB_PASSWORD
+KRB_CIPHER=aes256-cts-hmac-sha1-96
+
+if [ -z "$KRB_PASSWORD" -o -z "$KRB_REALMS" ] ; then usage ; fi
+
+for realm in $KRB_REALMS ; do
+	echo "addent -password -p $KRB_USERNAME@$realm -k 1 -e $KRB_CIPHER\n$KRB_PASSWORD\nwrite_kt $KRB_USERNAME.keytab" | ktutil
+done

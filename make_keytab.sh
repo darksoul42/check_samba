ebfb6208100ac874984345916806ed0c1345baf8
Stéphane Lapie <darksoul@darkbsd.org>
2021-11-06 19:16:35 +0900

    Made it possible to specify the destination file for the keytab to create

--- make_keytab.sh
+++ make_keytab.sh
@@ -5,12 +5,15 @@ SCRIPT=`basename $0`
 
 usage()
 {
-	echo "Usage: KRB5_CONFIG=/path/krb5.conf $0 user [<REALM1..N>] < password-from-stdin"
+	echo "Usage: KRB5_CONFIG=/path/krb5.conf $0 <USER> <DESTINATION_FILE> [<REALM1..N>] < password-from-stdin"
 	exit 1
 }
 
 KRB_USERNAME=$1
 if [ -z "$KRB_USERNAME" ] ; then usage ; fi
+shift
+KRB_KEYTAB=$1
+if [ -z "$KRB_KEYTAB" ] ; then usage ; fi
 shift
 KRB_REALMS=$@
 read KRB_PASSWORD
@@ -19,5 +22,5 @@ if [ -z "$KRB_CIPHER" ] ; then KRB_CIPHER=aes256-cts-hmac-sha1-96 ; fi
 if [ -z "$KRB_PASSWORD" -o -z "$KRB_REALMS" ] ; then usage ; fi
 
 for realm in $KRB_REALMS ; do
-	printf "%b" "addent -password -p $KRB_USERNAME@$realm -k 1 -e $KRB_CIPHER\n$KRB_PASSWORD\nwrite_kt $KRB_USERNAME.keytab" | ktutil
+	printf "%b" "addent -password -p $KRB_USERNAME@$realm -k 1 -e $KRB_CIPHER\n$KRB_PASSWORD\nwrite_kt $KRB_KEYTAB" | ktutil
 done
